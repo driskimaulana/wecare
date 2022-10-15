24b71c59ec40be274a9010bdcd940f62f8fcac8d
Hafizh Lutfi Hidayat <hafizh.lutfihidayat@upi.edu>
2022-10-15 19:28:21 +0700

    modification to ElderHome and cleanup

--- app/src/main/java/com/kelompok4/wecare/ElderHome.java
+++ app/src/main/java/com/kelompok4/wecare/ElderHome.java
@@ -2,15 +2,26 @@ package com.kelompok4.wecare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.kelompok4.wecare.databinding.ActivityElderHomeBinding;
 
 public class ElderHome extends AppCompatActivity {
+    private ActivityElderHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_elder_home);
 
-        
+        binding = ActivityElderHomeBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    public void qrBtnClicked(View v) {
+        Intent intent =  new Intent(this, ElderAccountConnection.class);
+        startActivity(intent);
     }
 }
