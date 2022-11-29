027973fa82297640f8334e4408a8557c124f4006
Hafizh Lutfi Hidayat <hafizh.lutfihidayat@upi.edu>
2022-11-29 14:49:45 +0700

    fix merging issues

--- app/src/main/java/com/kelompok4/wecare/view/elder/ElderQrCodeFragment.java
+++ app/src/main/java/com/kelompok4/wecare/view/elder/ElderQrCodeFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,9 +16,14 @@ import com.google.zxing.BarcodeFormat;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 import com.kelompok4.wecare.R;
 import com.kelompok4.wecare.databinding.FragmentElderQrCodeBinding;
+import com.kelompok4.wecare.model.user.User;
+import com.kelompok4.wecare.viewmodel.utils.GsonUtils;
 
 public class ElderQrCodeFragment extends Fragment {
     private FragmentElderQrCodeBinding binding;
+    private User currentUser;
+
+
 
     public ElderQrCodeFragment() {
         // Required empty public constructor
@@ -37,9 +43,21 @@ public class ElderQrCodeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Bundle bundle = getActivity().getIntent().getExtras();
+//        currentUser = GsonUtils.getGson().fromJson(bundle.getString("USER_LOGGED_IN"), User.class);
+//
+//        if (currentUser != null) {
+//            binding.elderName.setText(currentUser.getName());
+//        }
+
+//        get current logged in userW
+        currentUser = GsonUtils.getGson().fromJson(bundle.getString("USER_LOGGED_IN"), User.class);
+
+        String urlConnect = "https://api-wecare-mobapp.herokuapp.com/user/connect/" + currentUser.getId();
+        Toast.makeText(getActivity(), urlConnect, Toast.LENGTH_SHORT).show();
         try {
             BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-            Bitmap bitmap = barcodeEncoder.encodeBitmap("elder_placeholder_uid",
+            Bitmap bitmap = barcodeEncoder.encodeBitmap(urlConnect,
                     BarcodeFormat.QR_CODE, 400, 400);
             ImageView imageViewQrCode = (ImageView) view.findViewById(R.id.img_qrcode);
             imageViewQrCode.setImageBitmap(bitmap);
